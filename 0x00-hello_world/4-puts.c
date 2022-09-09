376fee79d80b89b6b12ec0d16b4c9e22c4f5725f
edwinkmusaasizi <edwinkmusaasizi@gmail.com>
2022-09-09 05:22:08 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the put funtion
+ *
+ * Descrition: using thee main function
+ * this program prints "programming is lie a multilingual puzzle
+ * Return: 0
+ * /
+ int main(void)
+ {
+ 	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
