0ffe8793b94e02710996cb0420369ca7a99fe8ea
edwinkmusaasizi <edwinkmusaasizi@gmail.com>
2022-09-09 07:06:38 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - print the string in the put funtion
  *
@@ -7,8 +6,8 @@
  * this program prints programming is lie a multilingual puzzle
  * Return: 0
  */
- int main(void)
- {
-	 puts("\"Programming is like building a multilingual puzzle");
-	 return (0);
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
