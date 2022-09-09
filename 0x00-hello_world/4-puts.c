6c1d2396623d51c81808f82621782282b459763a
edwinkmusaasizi <edwinkmusaasizi@gmail.com>
2022-09-09 06:56:49 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * main - print the string in the put funtion
  *
  * Descrition: using thee main function
- * this program prints "programming is lie a multilingual puzzle
+ * this program prints programming is lie a multilingual puzzle
  * Return: 0
  */
  int main(void)
