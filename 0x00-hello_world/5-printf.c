3868a82359926e9db963186b4da92d51c67cb829
edwinkmusaasizi <edwinkmusaasizi@gmail.com>
2022-09-09 06:26:41 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the put function 
- *
+ * main - print the string in the put function
  * Description: using the main function 
  * this program prints "with proper grammar, but the outcome is a piece of art
  * Return: 0
