57d88a90d82db04daca389302916ddc26b3f11a8
edwinkmusaasizi <edwinkmusaasizi@gmail.com>
2022-09-12 04:55:53 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - start
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 'o'; d <= '9'; d++)
+	{
+		putchar(d);
+		if (d != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
