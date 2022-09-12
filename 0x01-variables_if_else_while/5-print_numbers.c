4e839a6ca93354661fa6cffa6deecc88c65dd815
edwinkmusaasizi <edwinkmusaasizi@gmail.com>
2022-09-12 04:16:50 -0700

    5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,12 +8,13 @@
  */
 int mian(void)
 {
-	int i;
-	
-	for (i = '0'; i < '10'; i++)
+	char i;
+
+	for (i='0';i<='9';i++)
 	{
-		printf("%d", i);
+		putchar(i);
 	}
-	printf("\n");
-	return (0);
-}
+
+	putchar('\n');
+
+	return (0)
