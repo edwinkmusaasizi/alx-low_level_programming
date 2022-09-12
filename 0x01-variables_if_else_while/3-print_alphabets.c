4716b905a436255d5d272e68f68a83a60a89a959
edwinkmusaasizi <edwinkmusaasizi@gmail.com>
2022-09-12 03:16:14 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
+
 	char ch;
 
 	for (ch = 'a' ; ch <= 'z' ; ch++)
-	{
+
 		putchar(ch);
-	}
-	for (ch = 'A' ; ch <= 'Z' ; ch++)
-	{
-		putchar(ch);
-	}
-	putcar('\n');
+	for (ch = 'A' ; ch <= '' ; ch++)
+		putchar(ch); 
+		putchar('\n');
 	return (0);
+
+
 }
