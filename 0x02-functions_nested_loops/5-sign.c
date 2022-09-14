c6480c351d8d6ae2356522fd3581639568013320
edwinkmusaasizi <edwinkmusaasizi@gmail.com>
2022-09-14 05:26:25 -0700

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,8 @@
  * print_sign - function tocheck for a sign of a number
  * @n: is the int that will use for the argument of the function
  * Return: 0
- */int print_sign(int n)
+ */
+int print_sign(int n)
 {
 	if (n > 0)
 	{
