c49faa25a85bc8aaf927d948c4fd519a302a725a
edwinkmusaasizi <edwinkmusaasizi@gmail.com>
2022-09-16 05:05:36 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - program that prints either number
@@ -8,34 +9,24 @@
 
 int main(void)
 {
-		int num = 1;
+	int i;
+	char f[] = "Fizz";
+	char b[] = "Buzz";
+	char fb[] = "FizzBuzz";
 
-		while (num++ < 100)
-		{
-			if ((num % 3 == 0) && (num % 5 == 0))
-			{
-					printf("FizzBuzz");
-			}
-			else if ((num % 3) == 0)
-			{
-				printf("Fizz");
-			}
-			else if ((num % 5) == 0)
-			{
-				if (num != 100)
-				{
-					printf("Buzz");
-				}
-				else
-				{
-					printf("Buzz");
-				}
-			}
-			else
-			{
-				printf("%d ", num);
-			}
-		}
-			printf("\n");
-			return (0);
+	for (i = 1; i <= 100; i++)
+	{
+		if (i == 100)
+			printf("%s", b);
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("%s ", fb);
+		else if (i % 3 == 0)
+			printf("%s ", f);
+		else if (i % 5 == 0)
+			printf("%s ", b);
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
 }
