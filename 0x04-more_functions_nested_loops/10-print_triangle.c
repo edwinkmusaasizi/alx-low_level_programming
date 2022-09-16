aa2979431ea0573d6db0ddfcc363c435888eb540
edwinkmusaasizi <edwinkmusaasizi@gmail.com>
2022-09-16 02:59:55 -0700

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -19,10 +19,9 @@ void print_triangle(int size)
 			{
 				putchar(' ');
 			}
-			
 			for (inc2 = 0; inc2 < inc1; inc2++)
 			{
-					putchar('#');
+				putchar('#');
 			}
 			if (inc1 == size)
 			{
