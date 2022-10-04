ac46383fd08adafc1a13e22b449eb1bc07efcddb
edwinkmusaasizi <edwinkmusaasizi@gmail.com>
2022-10-04 01:51:55 -0700

    5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -5,6 +5,7 @@
  *             arguments are separated by a new line in the string.
  * @ac: The number of arguments passed to the program.
  * @av: An array of pointers to the arguments.
+ *
  * Return: If ac == 0, av == NULL, or the function fails - NULL.
  *         Otherwise - a pointer to the new string.
  */
@@ -26,18 +27,18 @@ char *argstostr(int ac, char **av)
 	if (str == NULL)
 		return (NULL);
 
-		index = 0;
+	index = 0;
 
-		for (arg = 0; arg < ac; arg++)
-		{
-			for (byte = 0; av[arg][byte]; byte++)
-				str[index++] = av[arg][byte];
-			
-			str[index++] = '\n';
-		}
-		
-		str[size] = '\0';
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (byte = 0; av[arg][byte]; byte++)
+			str[index++] = av[arg][byte];
 
-		return (str);
+		str[index++] = '\n';
+	}
+
+	str[size] = '\0';
+
+	return (str);
 }
 
